7a8164b10b6de9985cca414433987e2dd5600bd0
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-11-13 14:15:32 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,27 +1,32 @@
 #!python3
 
 """
-Create a list that contains the following strings, in order:
-Cat
-Fish
-Dog
-Bear
-Turtle
-
-Sort the list into alphabetical order and and then ask the user to enter a number corresponding
-to the index of an element.  Print the element associated with that index.
-
+Create a variable that contains an empy list.
+Ask a user to enter 5 words.  Add the words into the list.
+Print the list
 inputs:
-integer number
+string 
+string
+string
+string
+string
 
 outputs:
-string animal
+string
 
 example:
-Enter the index for an animal:2
-The animal at that index is Dog
+Enter a word: apple
+Enter a word: worm
+Enter a word: dollar
+Enter a word: shingle
+Enter a word: virus
+
+['apple', 'worm', 'dollar', 'shingle', 'virus']
 """
+a = input("Enter a word:")
+b = input("Enter a word:")
+c = input("Enter a word:")
+d = input("Enter a word:")
+e = input("Enter a word:")
 
-
-
-animals =
+print([a,b,c,d,e])
