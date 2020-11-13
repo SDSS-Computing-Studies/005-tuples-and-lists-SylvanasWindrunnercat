e3112f47890e6c00444b303f5bc62a6711131122
SylvanasWindrunnercat <71406931+SylvanasWindrunnercat@users.noreply.github.com>
2020-11-13 14:16:43 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -19,3 +19,10 @@ Enter the replacement:Dan
 ['Alain', 'Brian', 'Chris', 'Justin', 'Angela', 'Dan']
 
 """
+myList = ["Alain","Brian","Chris","Justin","Angela","Rick"]
+
+a = input("Choose a person from the list to replace:")
+
+myList.remove(a)
+
+print(myList)
